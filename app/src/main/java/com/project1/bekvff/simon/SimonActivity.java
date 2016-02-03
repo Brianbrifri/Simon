0ec169884b2187362fc1765f836304ff0c391c4a
Brian K <brianbrifri@gmail.com>
2016-02-03 12:51:13 -0600

    First Commit

---
+++ app/src/main/java/com/project1/bekvff/simon/SimonActivity.java
@@ -0,0 +1,13 @@
+package com.project1.bekvff.simon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simon);
+    }
+}
